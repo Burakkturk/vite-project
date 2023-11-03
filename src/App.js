4d64ee5b69c545d2386318659435820208e904b1
Burak <burakturk@gmail.com>
2023-11-03 03:48:52 +0300

    columns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bulma/css/bulma.css";
 import Course from "./Course";
 import Angular from "./images/angular.jpg";
 import Bootstrap from "./images/bootstrap5.png";
@@ -7,30 +8,45 @@ import KompleWeb from "./images/kompleweb.jpg";
 function App() {
   return (
     <div className="App">
-      <Course
-        image={Angular}
-        title="Angular"
-        description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
+      <div className="container">
+        <div className="columns">
+          <div className="column"></div>
+          <div className="column">
+            <Course
+              image={Angular}
+              title="Angular"
+              description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
     "
-      />
-      <Course
-        image={Bootstrap}
-        title="Bootstrap"
-        description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Bootstrap}
+              title="Bootstrap"
+              description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
 "
-      />
-      <Course
-        image={CCsharp}
-        title="Komple Web"
-        description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
+            />
+          </div>
+
+          <div className="column">
+            <Course
+              image={CCsharp}
+              title="Komple Web"
+              description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
     "
-      />
-      <Course
-        image={KompleWeb}
-        title="Front-End"
-        description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
+            />
+          </div>
+
+          <div className="column">
+            <Course
+              image={KompleWeb}
+              title="Front-End"
+              description="afedhdlşhinkrfdoikxjniofxknox pxdkhğpdxkhbğp ipxdkhnğpdzkbnğp
     "
-      />
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
